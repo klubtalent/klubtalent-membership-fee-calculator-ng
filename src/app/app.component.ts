ddff349fcb45ad50f8f3233a1a62f7ae80c6aa00
Florian Schwanz <florian.schwanz@interoberlin.de>
2022-05-01 16:57:53 +0200

    chore: update styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {MatIconRegistry} from "@angular/material/icon";
+import {MaterialIconService} from "./core/ui/services/material-icon.service";
+import {DomSanitizer} from "@angular/platform-browser";
+import {environment} from "../environments/environment";
+import {MaterialColorService} from "./core/ui/services/material-color.service";
 
 /**
  * Displays app component
@@ -8,8 +13,30 @@ import {Component} from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
-  /** Default app theme */
-  public themeClass = 'klubtalent-blue-theme';
+  /**
+   * Constructor
+   * @param iconRegistry icon registry
+   * @param materialIconService material icon service
+   * @param materialColorService material color service
+   * @param sanitizer sanitizer
+   */
+  constructor(private iconRegistry: MatIconRegistry,
+              private materialIconService: MaterialIconService,
+              public materialColorService: MaterialColorService,
+              private sanitizer: DomSanitizer) {
+  }
+
+  //
+  // Lifecycle hooks
+  //
+
+  /**
+   * Handles on-init lifecycle phase
+   */
+  ngOnInit() {
+    this.materialIconService.initializeIcons(this.iconRegistry, this.sanitizer);
+    this.materialColorService.initializeTheme(environment.theme);
+  }
 }
