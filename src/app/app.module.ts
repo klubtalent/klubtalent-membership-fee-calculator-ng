4494eddf635d24b7c9a1f5a532cf623696e0db5c
Florian Schwanz <florian.schwanz@interoberlin.de>
2022-05-03 11:34:20 +0200

    feat: turn app into PWA

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {MatToolbarModule} from "@angular/material/toolbar";
 import {UiModule} from "./core/ui/ui.module";
+import {ServiceWorkerModule} from '@angular/service-worker';
+import {environment} from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -17,7 +19,13 @@ import {UiModule} from "./core/ui/ui.module";
 
     MatToolbarModule,
 
-    AppRoutingModule
+    AppRoutingModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
